2c1151592aa70d2e1b218922a9ca25001b6583ed
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-03-16 13:37:42 -0600

    guts

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 class Signup extends Component {
     state = {
-        validUsername: false,
-        validPassword: false,
-        confirmPassword: false
+        validUsername: true,
+        validPassword: true,
+        confirmPassword: true
     }
     
     componentDidUpdate() {
@@ -79,11 +79,11 @@ class Signup extends Component {
                         <FormText>at least 8 characters, 1 capital & 1 number</FormText>
                     </FormGroup>
                     {/* if all fields are valid, allow the user to submit the form */}
-                    {(this.state.validUsername && this.state.validPassword && this.state.confirmPassword) ? (
+                    {/* {(this.state.validUsername && this.state.validPassword && this.state.confirmPassword) ? ( */}
                         <Button onClick={this.props.handleSignup} color="success" block>Signup</Button>
-                    ) : (
-                        <Button onClick={this.props.handleSignup} color="danger" block disabled>Signup</Button>
-                    )}
+                    {/* ) : ( */}
+                        {/* <Button onClick={this.props.handleSignup} color="danger" block disabled>Signup</Button> */}
+                    {/* )} */}
                     <p className="signupLink">
                         <Link to="/login">already have an account?  Sign in here</Link>
                     </p>
